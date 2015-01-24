af3064d53db363fd2f46305c7148d0d6c86566b2
Johan <dev@tencc.net>
2015-01-24 08:24:01 +0100

    A corriger, il y un bug.

--- read_image.c
+++ read_image.c
@@ -3,48 +3,96 @@
 #include <stdlib.h>
 #include <string.h>
 #include <strings.h>
+#include <math.h>
 
 #include "rdjpeg.h"
 
-int main(int argc, char *argv[])
-{
-  int i,j,n,nx,ny,nb;
-  CIMAGE cim;
-  /*------------------------------------------------*/
-  /* lecture d'une image requête                    */
-  /*------------------------------------------------*/
-  read_cimage(argv[1],&cim);
-  /*------------------------------------------------*/
-  /* affichage des valeurs pour le premier bloc 8x8 */
-  /* comme exemple de traitement                    */
-  /*------------------------------------------------*/
-  printf("Largeur de l'image : %d\n",cim.nx);
-  printf("Heuteur de l'image : %d\n",cim.ny);
-  printf("Plan rouge du premier bloc 8x8 :\n");
-  for (j = 0; j < 8; j++) {       /* ligne par ligne */
-    printf("  ");
-    for (i = 0; i < 8; i++) {   /* pixel par pixel */
-      printf("%4d",cim.r[i][j]);
-    }
-    printf("\n");
-  }
-  printf("Plan vert du premier bloc 8x8 :\n");
-  for (j = 0; j < 8; j++) {       /* ligne par ligne */
-    printf("  ");
-    for (i = 0; i < 8; i++) {   /* pixel par pixel */
-      printf("%4d",cim.g[i][j]);
-    }
-    printf("\n");
-  }
-  printf("Plan bleu du premier bloc 8x8 :\n");
-  for (j = 0; j < 8; j++) {       /* ligne par ligne */
-    printf("  ");
-    for (i = 0; i < 8; i++) {   /* pixel par pixel */
-      printf("%4d",cim.b[i][j]);
-    }
-    printf("\n");
-  }
-  /*------------------------------------------------*/
+#define BINS 4
 
-  exit(0);
+void print_histogram (char *argv[]) {
+	int i,j,k,l,n,nx,ny,nb;
+	CIMAGE cim;
+	/*------------------------------------------------*/
+	/* lecture d'une image requï¿½te                    */
+	/*------------------------------------------------*/
+	read_cimage(argv[1],&cim);
+	/*------------------------------------------------*/
+	/* affichage des valeurs pour le premier bloc 8x8 */
+	/* comme exemple de traitement                    */
+	/*------------------------------------------------*/
+	printf("Largeur de l'image : %d\n",cim.nx);
+	printf("Heuteur de l'image : %d\n",cim.ny);
+
+	float h[BINS*BINS*BINS] = {};
+
+	for (j = 0; j < cim.nx; j++) {       /* ligne par ligne */
+		for (i = 0; i < cim.ny; i++) {   /* pixel par pixel */
+			int r = ((int) cim.r[i][j] * BINS) / 256;
+			int g = ((int) cim.g[i][j] * BINS) / 256;
+			int b = ((int) cim.b[i][j] * BINS) / 256;
+			int k = r + BINS*g + BINS*BINS*b;
+			h[k] += 1.0;
+		}
+	}
+
+	k=0;
+	for (l = 0 ; l < BINS; l++) {
+		for (j = 0 ; j < BINS; j++) {
+			for (i = 0 ; i < BINS; i++) {
+				printf("%f ",h[k] / ((float) cim.nx*cim.ny));
+				k++;
+			}
+			printf("\n");
+		}
+		printf("\n");
+	}
+
+}
+
+void print_color_plan (char *argv[]) {
+	int i,j,n,nx,ny,nb;
+	CIMAGE cim;
+	/*------------------------------------------------*/
+	/* lecture d'une image requï¿½te                    */
+	/*------------------------------------------------*/
+	read_cimage(argv[1],&cim);
+	/*------------------------------------------------*/
+	/* affichage des valeurs pour le premier bloc 8x8 */
+	/* comme exemple de traitement                    */
+	/*------------------------------------------------*/
+	printf("Largeur de l'image : %d\n",cim.nx);
+	printf("Heuteur de l'image : %d\n",cim.ny);
+	printf("Plan rouge du premier bloc 8x8 :\n");
+	for (j = 0; j < 8; j++) {       /* ligne par ligne */
+		printf("  ");
+		for (i = 0; i < 8; i++) {   /* pixel par pixel */
+			printf("%4d",cim.r[i][j]);
+		}
+		printf("\n");
+	}
+	printf("Plan vert du premier bloc 8x8 :\n");
+	for (j = 0; j < 8; j++) {       /* ligne par ligne */
+		printf("  ");
+		for (i = 0; i < 8; i++) {   /* pixel par pixel */
+			printf("%4d",cim.g[i][j]);
+		}
+		printf("\n");
+	}
+	printf("Plan bleu du premier bloc 8x8 :\n");
+	for (j = 0; j < 8; j++) {       /* ligne par ligne */
+		printf("  ");
+		for (i = 0; i < 8; i++) {   /* pixel par pixel */
+			printf("%4d",cim.b[i][j]);
+		}
+		printf("\n");
+	}
+	/*------------------------------------------------*/
+}
+
+int main(int argc, char *argv[]) {
+
+	print_histogram(argv);
+
+	//print_color_plan(argv);
+	exit(0);
 }
